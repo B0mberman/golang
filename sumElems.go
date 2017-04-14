7ae1cc4e6f507c406fd4d90168deced103c14de2
B0mberman <b0mbrmn@yandex.ru>
2017-04-15 02:03:02 +0300

    Сумма элементов массива по диагонали

---
+++ sumElems.go
@@ -0,0 +1,42 @@
+package main
+import (
+	"fmt"
+)
+func main() {
+	var linesNumber int
+	var elemsNumber int
+	var number int
+	var sum int
+
+	fmt.Println("Количество строк?")
+	fmt.Scanf("%d", &linesNumber)
+	fmt.Println("Количество элементов в строке?")
+	fmt.Scanf("%d", &elemsNumber)
+
+	list := make([][]int, linesNumber)
+
+	for i := range list {
+		list[i] = make([]int, elemsNumber)
+	}
+
+	for i := 0; i < linesNumber; i++ {
+		for j := 0; j < elemsNumber; j++ {
+			fmt.Println("Введите число")
+			fmt.Scanf("%d", &number)
+			list = append(list, []int{0,0})
+			list[i][j] = number
+		}
+	}
+
+	for i := 0; i < linesNumber; i++ {
+		for j := 0; j < elemsNumber; j++ {
+			if j == i {
+				sum = sum + list[i][j]
+			}
+		}
+	}
+
+
+	fmt.Println(sum)
+
+}
